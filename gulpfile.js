6def1a13a85f4c065b3985ee7fbf81151217210e
tis <tisbootis@gmail.com>
2020-09-15 22:39:40 +0300

    add tasks for custom svg-icons (svg-sprite)

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,10 @@ const imagemin = require("gulp-imagemin");
 const svgstore = require("gulp-svgstore");
 const webp = require("gulp-webp");
 
+// const globParent = require('glob-parent');
+// const path = require("path");
+// const tap = require("gulp-tap");
+
 const pug = require("gulp-pug");
 const formatHTML = require('gulp-format-html')
 
@@ -36,22 +40,22 @@ const formatHTML = require('gulp-format-html')
 //     .pipe(server.stream());
 // });
 
-gulp.task("css", function () {
+gulp.task("css", function() {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
-    // .pipe(sourcemap.init())             //*min
+    .pipe(sourcemap.init()) //*min
     .pipe(less())
     .pipe(postcss([autoprefixer()]))
     // .pipe(gulp.dest("source/css"))      //*source
     // .pipe(cleanCSS({level: 2}))         //*min
     // .pipe(rename("style.min.css"))      //*min
-    // .pipe(sourcemap.write("."))         //*min
-    .pipe(gulp.dest("source/css"))      //*source
+    .pipe(sourcemap.write(".")) //*min
+    .pipe(gulp.dest("source/css")) //*source
     // .pipe(gulp.dest("build/css"))       //*build
     .pipe(server.stream());
 });
 
-gulp.task("js", function () {
+gulp.task("js", function() {
   return gulp.src("source/js/main.js")
     .pipe(uglify({
       toplevel: true
@@ -60,32 +64,7 @@ gulp.task("js", function () {
     .pipe(server.stream());
 });
 
-gulp.task("img", function () {
-  return gulp.src("source/img/z/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("webp", function () {
-  return gulp.src("source/img/z/*")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img/webp"));
-});
-
-gulp.task("sprite", function () {
-  return gulp.src("source/img/**/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("server", function () {
+gulp.task("server", function() {
   server.init({
     server: "source/",
     notify: false,
@@ -101,3 +80,78 @@ gulp.task("server", function () {
 });
 
 gulp.task("default", gulp.series("css", "server"));
+
+// опциональные таски
+
+gulp.task("img", function() {
+  return gulp.src("source/img/z/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/z/*")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img/webp"));
+});
+
+function catchPath(file, t) {
+  return file.base;
+}
+
+function catchLastWord(el) {
+  return el.replace(/^.*\\/,'')
+}
+
+gulp.task('svg-rename', function() {
+  let fullPath,
+      parentPath;
+  return gulp.src("source/img/icons/**/*.svg")
+
+    // .pipe(tap(function(file, t) {
+    //   fullPath = file.base;
+    //   console.log(file.base);
+
+    //   // catchLastWord(fullPath)
+    //   return (fullPath)
+    // }))
+
+    .on('data', function(file){
+      // console.log({
+      //   contents: file.contents,
+      //   path: file.path,
+      //   cwd: file.cwd,
+      //   base: file.base,
+      //   relative: file.relative,
+      //   dirname: file.dirname,
+      //   basename: file.basename,
+      //   stem: file.stem,
+      //   extname: file.extname
+      // });
+
+      // fullPath = file.base;
+      fullPath = file.dirname;
+    })
+
+    .pipe(rename(function (file) {
+      parentPath = catchLastWord(fullPath);
+      file.basename = parentPath + '-' + file.basename;
+    }))
+
+    .pipe(gulp.dest("source/img/icons-rename"));
+});
+
+gulp.task('svg-sprite', function() {
+  return gulp.src("source/img/icons-rename/**/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("svg", gulp.series("svg-rename", "svg-sprite"));
